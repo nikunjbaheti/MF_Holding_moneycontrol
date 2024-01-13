6d53552d757621ee1a2bf1166b03d10bd6d7fd9e
Nikunj Baheti <nikunj.baheti@gmail.com>
2024-01-13 09:37:15 +0530

    Update createBulkCsv.py

--- src/createBulkCsv.py
+++ src/createBulkCsv.py
@@ -1,46 +1,39 @@
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
-import string
 
-#Format for URL : http://www.moneycontrol.com/india/mutualfunds/mfinfo/portfolio_holdings/MBO225
-def cleanTableRecord(tableRecord):
-     # cleanRecord=tableRecord.text.translate(None,string.whitespace)
-     # cleanRecord=cleanRecord.text.translate('','-')
-     to_remove = {'-','\n'}
-     table = {ord(char): None for char in to_remove}
-     cleanRecord = tableRecord.text.translate(table)
-     cleanRecord=cleanRecord.strip()
-     return cleanRecord
+# Format for URL: http://www.moneycontrol.com/india/mutualfunds/mfinfo/portfolio_holdings/MBO225
+def clean_table_record(table_record):
+    to_remove = {'-', '\n'}
+    table = {ord(char): None for char in to_remove}
+    clean_record = table_record.text.translate(table)
+    clean_record = clean_record.strip()
+    return clean_record
 
-
-def createCsvForRow(row):
-    
-    url = "http://www.moneycontrol.com/india/mutualfunds/mfinfo/portfolio_holdings/"+row['URL Tag']
-    print(f'Plan :'+ row['URL Tag'] +' - URL :'+url)
+def create_csv_for_row(row):
+    url = "http://www.moneycontrol.com/india/mutualfunds/mfinfo/portfolio_holdings/" + row['URL Tag']
+    print(f'Plan: {row["URL Tag"]} - URL: {url}')
     response = requests.get(url)
-    soup = BeautifulSoup(response.text,"html.parser")
-    table=soup.find(id="equityCompleteHoldingTable")
-    if(table!=None):
-        #Headers
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find(id="equityCompleteHoldingTable")
+    
+    if table is not None:
+        # Headers
         headers = [header.text for header in table.find_all('th')]
-        rows=[]
-        #Get All rows
+        rows = []
+        # Get all rows
         for holding in table.find_all('tr'):
-             rows.append([cleanTableRecord(val)  for val in holding.find_all('td')])
+            rows.append([clean_table_record(val) for val in holding.find_all('td')])
 
-        destinationPath='csv/'+row['URL Tag']+'.csv'
-      #Write
-        with open(destinationPath, 'w',newline='') as f:
+        destination_path = f'csv/{row["URL Tag"]}.csv'
+        # Write
+        with open(destination_path, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(headers)
             writer.writerows(row for row in rows)
 
-df = pd.read_csv('csv/FundList.csv',delimiter=',')
+df = pd.read_csv('csv/FundList.csv', delimiter=',')
 
 for i, row in df[::-1].iterrows():
-    createCsvForRow(row)
-
+    create_csv_for_row(row)
