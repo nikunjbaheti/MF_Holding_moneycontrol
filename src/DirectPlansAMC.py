655cf19f486b5d179340a37b982b4541a1625659
Nikunj Baheti <nikunj.baheti@gmail.com>
2024-01-13 09:36:32 +0530

    Update DirectPlansAMC.py

--- src/DirectPlansAMC.py
+++ src/DirectPlansAMC.py
@@ -1,42 +1,30 @@
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 import csv
-import string
-import re
 
-
-mainURL="https://www.moneycontrol.com/mutualfundindia/"
+mainURL = "https://www.moneycontrol.com/mutualfundindia/"
 response = requests.get(mainURL)
-soup = BeautifulSoup(response.text,"lxml")
-mfRows=[]
-links=[]
-# Get Holdings Table
-amcClass=soup.findAll(attrs={'class':'amclink'})
-for div in amcClass:
-    links = div.findAll('a')
+soup = BeautifulSoup(response.text, "lxml")
+mfRows = []
 
+# Get AMC links
+amcClass = soup.find_all(attrs={'class': 'amclink'})
+for div in amcClass:
+    links = div.find_all('a')
 
 for amc in links:
-     #Trying out for Franklin - Getting equity 
-     
-     url = amc.get('href')
-     print("Adding for"+url)
-     response = requests.get(url)
+    # Trying out for Franklin - Getting equity
+    url = amc.get('href')
+    print("Adding for" + url)
+    response = requests.get(url)
+    soupAmc = BeautifulSoup(response.text, "html.parser")
 
-     soupAmc = BeautifulSoup(response.text,"html.parser")
+    allFundsForAMC = soupAmc.find_all("a", attrs={'class': 'arial11blue'})
+    for row in allFundsForAMC:
+        mfRows.append([row.text, row.get('href').split('/')[-1]])
 
-     
-
-     allFundsForAMC=soupAmc.findAll("a",attrs={'class':'arial11blue'})
-     for row in allFundsForAMC:
-          mfRows.append([row.text,row.get('href').split('/')[-1]])
-
-
-headers=["Fund Name","URL Tag"]
-with open('csv/FundList.csv','w',newline='\n') as f:
+headers = ["Fund Name", "URL Tag"]
+with open('csv/FundList.csv', 'w', newline='\n') as f:
     writer = csv.writer(f)
     writer.writerow(headers)
     writer.writerows(row for row in mfRows)
-
